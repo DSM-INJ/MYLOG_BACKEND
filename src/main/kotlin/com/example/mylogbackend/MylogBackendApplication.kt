a5aec619bbf14e1b1f96eb45ef9e0b153a1351c4
주영 <jyk102999@dsm.hs.kr>
2023-07-01 20:56:06 +0900

    ⚙ :: 프로젝트 세팅

---
+++ src/main/kotlin/com/example/mylogbackend/MylogBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mylogbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MylogBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<MylogBackendApplication>(*args)
+}

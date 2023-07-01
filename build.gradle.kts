5560e1fad5cf8cf70a0ed3f1580d07d9464b79d8
주영 <jyk102999@dsm.hs.kr>
2023-07-01 21:31:52 +0900

    ⚙️ :: 의존성 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,66 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.1.1"
-    id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
+	id("org.springframework.boot") version PluginVersion.SPRING_BOOT_FRAMEWORK_VERSION
+	id("io.spring.dependency-management") version PluginVersion.SPRING_DEPENDENCY_MANAGEMENT_VERSION
+	kotlin("jvm") version PluginVersion.JVM_VERSION
+	kotlin("plugin.spring") version PluginVersion.PLUGIN_SPRING_VERSION
+	kotlin("plugin.jpa") version PluginVersion.PLUGIN_JPA_VERSION
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-}
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation(Dependency.JPA)
+	implementation(Dependency.SECURITY)
+	implementation(Dependency.VALIDATION)
+	implementation(Dependency.WEB)
+	implementation(Dependency.JACKSON)
+	implementation(Dependency.REFLECT)
+	implementation(Dependency.JDK8)
+	implementation(Dependency.JWT)
+	runtimeOnly(Dependency.MYSQL)
+	implementation(Dependency.REDIS)
+	implementation(Dependency.OAUTH2)
+	implementation(Dependency.QUERYDSL)
+	implementation(Dependency.OPENFEIGN)
+	implementation(Dependency.CLOUD_AWS)
+	implementation(Dependency.EMAIL)
+	implementation(Dependency.SWAGGER)
+	implementation("org.springframework.security.oauth.boot", Dependency.OAUTH2_AUTOCONFIGURE, DependencyVersion.OAUTH2_AUTOCONFIGURE)
+	implementation("org.springframework.security", Dependency.OAUTH2_CLIENT, DependencyVersion.OAUTH2_CLIENT)
+	annotationProcessor(Dependency.CONFIGURATION_PROCESSOR)
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
-    }
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+	useJUnitPlatform()
+}
+
+allOpen {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.MappedSuperclass")
+	annotation("javax.persistence.Embeddable")
+}
+
+noArg {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.MappedSuperclass")
+	annotation("javax.persistence.Embeddable")
+}
+
+tasks.getByName<Jar>("jar") {
+	enabled = false
 }
---
+++ buildSrc/src/main/kotlin/DependencyVersion.kt
@@ -0,0 +1,9 @@
+object DependencyVersion {
+    const val JWT = "0.9.0"
+    const val OAUTH2_AUTOCONFIGURE = "2.6.8"
+    const val OAUTH2_CLIENT = "5.7.3"
+    const val QUERYDSL = "5.0.0"
+    const val OPENFEIGN = "3.1.4"
+    const val AWS ="2.2.6"
+    const val SWAGGER_VERSION = "3.0.0"
+}

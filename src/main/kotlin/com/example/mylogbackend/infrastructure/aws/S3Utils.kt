054b512d7fd2a72e64aa729e47d1668b8fc61f71
주영 <jyk102999@dsm.hs.kr>
2023-07-09 23:20:38 +0900

    📑 :: s3Utils 추가

---
+++ src/main/kotlin/com/example/mylogbackend/infrastructure/aws/S3Utils.kt
@@ -0,0 +1,42 @@
+package com.example.mylogbackend.infrastructure.aws
+
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.CannedAccessControlList
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model.PutObjectRequest
+import com.amazonaws.util.IOUtils
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import org.springframework.web.multipart.MultipartFile
+import java.io.ByteArrayInputStream
+import java.util.*
+
+class S3Utils(
+    private val s3Client: AmazonS3Client,
+
+    @Value("\${aws.s3.bucket}")
+    private val bucketName: String,
+
+    @Value("\${aws.s3.base-image-url}")
+    private val baseImageUrl: String,
+
+    @Value("\${aws.s3.prefix}")
+    private val prefix: String
+) {
+    fun upload(file: MultipartFile, path: String): String {
+        val fileName = UUID.randomUUID().toString() + "-" + file.originalFilename
+        val objMeta = ObjectMetadata()
+
+        val bytes = IOUtils.toByteArray(file.inputStream)
+        objMeta.contentLength = bytes.size.toLong()
+
+        val byteArrayIs = ByteArrayInputStream(bytes)
+
+        s3Client.putObject(
+            PutObjectRequest(bucketName, path + fileName, byteArrayIs, objMeta)
+                .withCannedAcl(CannedAccessControlList.PublicRead)
+        )
+
+        return s3Client.getUrl(bucketName, bucketName + fileName).toString()
+    }
+}

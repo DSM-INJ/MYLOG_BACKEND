fab831b15f58b8328f99be7b5ffeff6a9859231d
주영 <jyk102999@dsm.hs.kr>
2023-07-09 23:21:37 +0900

    ♻️ :: @EnableConfigurationProperties 추가

--- src/main/kotlin/com/example/mylogbackend/MylogBackendApplication.kt
+++ src/main/kotlin/com/example/mylogbackend/MylogBackendApplication.kt
@@ -1,8 +1,19 @@
 package com.example.mylogbackend
 
+import com.example.diaryservice.global.security.jwt.JwtProperties
+import com.example.oauth.infrastructure.feign.properties.FacebookFeignProperties
+import com.example.oauth.infrastructure.feign.properties.GoogleFeignProperties
+import com.example.oauth.infrastructure.feign.properties.KakaoFeignProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
+@EnableConfigurationProperties(
+    JwtProperties::class,
+    GoogleFeignProperties::class,
+    KakaoFeignProperties::class,
+    FacebookFeignProperties::class
+)
 @SpringBootApplication
 class MylogBackendApplication
 
